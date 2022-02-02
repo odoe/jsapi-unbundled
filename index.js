35008db4e0b4030ba087a16b980f598e407d42bb
Rene Rubalcava <rrubalcava@esri.com>
2022-02-02 12:52:07 -0800

    clean up

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-import esriConfig from "@arcgis/core/config.js";
 import WebMap from "@arcgis/core/WebMap.js";
 import MapView from "@arcgis/core/views/MapView.js";
 import Bookmarks from "@arcgis/core/widgets/Bookmarks.js";
 import Expand from "@arcgis/core/widgets/Expand.js";
-
-esriConfig.assetsPath = "./node_modules/@arcgis/core/assets";
 
 const webmap = new WebMap({
   portalItem: {
